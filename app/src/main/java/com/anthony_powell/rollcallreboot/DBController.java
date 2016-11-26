16cc1adebc9ef2ed4a7a90827ca549b821c5804f
antpowell <powell.anthony.e@gmail.com>
2016-11-26 11:02:20 -0600

    start setting up dbController

--- app/src/main/java/com/anthony_powell/rollcallreboot/DBController.java
+++ app/src/main/java/com/anthony_powell/rollcallreboot/DBController.java
@@ -1,8 +1,110 @@
 package com.anthony_powell.rollcallreboot;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.text.Layout;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 /**
  * Created by SGT_POWELL on 11/13/2016.
  */
 
 public class DBController  {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser dbUser;
+    private String dbLastName, dbEmail, dbPassword, dbStudentNumber;
+    private Context c;
+    private Boolean foundUser = Boolean.FALSE;
+
+    private DBController(){
+
+    }
+    private DBController(Context context){
+        this.c = context;
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public DBController(Context context, String dbLastName, String dbEmail, String dbPassword, String dbStudentNumber) {
+        this.c = context;
+        this.dbLastName = dbLastName;
+        this.dbEmail = dbEmail;
+        this.dbPassword = dbPassword;
+        this.dbStudentNumber = dbStudentNumber;
+
+    }
+
+    public void createUserWithEmail(final Layout layout){
+        mAuth.createUserWithEmailAndPassword(dbEmail, dbPassword).addOnCompleteListener(c, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                if(!task.isSuccessful()){
+                    Snackbar.make(layout,"Authentication failed", Snackbar.LENGTH_SHORT ).show();
+                }
+
+            }
+        });
+
+    }
+
+    public void createUserWithGmail(){
+
+    }
+
+    public Boolean hasUser(){return foundUser;}
+
+    public FirebaseUser getDbUser() {
+        mAuth.addAuthStateListener(mAuthListener);
+        return dbUser;
+    }
+
+    public String getDbLastName() {
+        return dbLastName;
+    }
+
+    public String getDbEmail() {
+        return dbEmail;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+
+    public String getDbStudentNumber() {
+        return dbStudentNumber;
+    }
+
+    public void userSignInOutListener(){
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser  user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    dbUser = user;
+                    foundUser = true;
+                }else{
+                    foundUser = false;
+                }
+
+            }
+        };
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    public void stopUserSignInOutListener(){
+     if(mAuthListener != null){
+         mAuth.removeAuthStateListener(mAuthListener);
+     }
+    }
 }
