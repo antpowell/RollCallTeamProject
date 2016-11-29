8301477df0ac1d59ac5906861426c7155fed6301
antpowell <powell.anthony.e@gmail.com>
2016-11-29 16:46:24 -0600

    clean up DBController and add a few more methods for creating and signing in and out with user as well as getting the course list

--- app/src/main/java/com/anthony_powell/rollcallreboot/DBController.java
+++ app/src/main/java/com/anthony_powell/rollcallreboot/DBController.java
@@ -29,130 +29,151 @@ import java.util.List;
  * Created by SGT_POWELL on 11/13/2016.
  */
 
-public class DBController  {
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private FirebaseUser dbUser;
-    private String dbLastName, dbEmail, dbPassword, dbStudentNumber;
-    private Context c;
-    private Boolean foundUser = Boolean.FALSE;
-    private ArrayList<String> courseCodes;
-    private FirebaseDatabase database;
-    private DatabaseReference mRef = database.getReference();
+public class DBController {
+ private FirebaseAuth mAuth;
+ private FirebaseAuth.AuthStateListener mAuthListener;
+ private FirebaseUser dbUser;
+ private String dbLastName, dbEmail, dbPassword, dbStudentNumber;
+ private Context c;
+ private Boolean foundUser = Boolean.FALSE;
+ private ArrayList<String> courseCodes;
+ private FirebaseDatabase database;
+ private DatabaseReference mRef = database.getReference();
+ private AuthResult authResult;
 
-    private DBController(){
+ private DBController() { }
 
-    }
-    private DBController(Context context){
-        this.c = context;
-        mAuth = FirebaseAuth.getInstance();
-    }
+ private DBController(Context context) {
+  this.c = context;
+  mAuth = FirebaseAuth.getInstance();
+ }
 
-    public DBController(Context context, String dbLastName, String dbEmail, String dbPassword, String dbStudentNumber) {
-        this.c = context;
-        this.dbLastName = dbLastName;
-        this.dbEmail = dbEmail;
-        this.dbPassword = dbPassword;
-        this.dbStudentNumber = dbStudentNumber;
+ public DBController(Context context, String dbLastName, String dbEmail, String dbPassword, String dbStudentNumber) {
+  this.c = context;
+  this.dbLastName = dbLastName;
+  this.dbEmail = dbEmail;
+  this.dbPassword = dbPassword;
+  this.dbStudentNumber = dbStudentNumber;
 
-    }
+ }
 
-    public void createUserWithEmail(){
-        mAuth.createUserWithEmailAndPassword(dbEmail, dbPassword).addOnCompleteListener((Activity) c, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+ /*
+ *****************************************All methods dealing with Users*************************************
+  */
+
+ public void createUserWithEmail() {
+  mAuth.createUserWithEmailAndPassword(dbEmail, dbPassword).addOnCompleteListener((Activity) c, new OnCompleteListener<AuthResult>() {
+   @Override
+   public void onComplete(@NonNull Task<AuthResult> task) {
 
 
-                if(!task.isSuccessful()){
-                    Toast.makeText(c, "Authentication failed", Toast.LENGTH_SHORT).show();
+    if (!task.isSuccessful()) {
+     Toast.makeText(c, "Authentication failed", Toast.LENGTH_SHORT).show();
 //                    Snackbar.make(layout,"Authentication failed", Snackbar.LENGTH_SHORT ).show();
-                }else{
-
-                }
-
-            }
-        });
-
+    } else {
+     Toast.makeText(c, "Welcome... New User: "+dbEmail, Toast.LENGTH_SHORT).show();
     }
 
-    public void createUserWithGmail(){
+   }
+  });
 
-    }
+ }
 
-    public Boolean hasUser(){return foundUser;}
+ public void createUserWithGmail() { }
 
-    public FirebaseUser getDbUser() {
-        mAuth.addAuthStateListener(mAuthListener);
-        return dbUser;
-    }
+ public Boolean hasUser() {
+  if(authResult.getUser()!=null){
+   foundUser = true;
+  }
 
-    public String getDbLastName() {
-        return dbLastName;
-    }
+  return foundUser;
+ }
 
-    public String getDbEmail() {
-        return dbEmail;
-    }
+ public FirebaseUser getDbUser() {
+  return authResult.getUser();
+ }
 
-    public String getDbPassword() {
-        return dbPassword;
-    }
+ public void signUserIn() {
+  mAuth.signInWithEmailAndPassword(dbEmail, dbPassword);
+ }
 
-    public String getDbStudentNumber() {
-        return dbStudentNumber;
-    }
+ public void signUserIn(String email, String password){
+  mAuth.signInWithEmailAndPassword(email, password);
+ }
 
-    public void userSignInOutListener(){
+ public void signUserOut() {
+  mAuth.signOut();
+ }
 
-        mAuth = FirebaseAuth.getInstance();
+ public void userSignInOutListener() {
 
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser  user = firebaseAuth.getCurrentUser();
-                if(user != null){
-                    dbUser = user;
-                    foundUser = true;
-                }else{
-                    foundUser = false;
-                }
+  mAuth = FirebaseAuth.getInstance();
 
-            }
-        };
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-    public void stopUserSignInOutListener(){
-     if(mAuthListener != null){
-         mAuth.removeAuthStateListener(mAuthListener);
-     }
-    }
+  mAuthListener = new FirebaseAuth.AuthStateListener() {
+   @Override
+   public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+    FirebaseUser user = firebaseAuth.getCurrentUser();
+    dbUser = user;
+   }
+  };
+  mAuth.addAuthStateListener(mAuthListener);
+ }
 
-    public void getCourseList(){
-        courseCodes = null;
+ public void stopUserSignInOutListener() {
+  if (mAuthListener != null) {
+   mAuth.removeAuthStateListener(mAuthListener);
+  }
+ }
 
-        final ProgressDialog progressDialog = new ProgressDialog(c);
+/*
+ *****************************************All methods dealing with Course Info*************************************
+  */
 
-        progressDialog.show();
-        mRef.child("Courses").child("Codes").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                courseCodes = (ArrayList<String>)dataSnapshot.getValue();
-                Toast.makeText(c, "Course codes found...", Toast.LENGTH_SHORT).show();
-                progressDialog.dismiss();
+ public void getCourseList() {
+  courseCodes = null;
 
-            }
+  final ProgressDialog progressDialog = new ProgressDialog(c);
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+  progressDialog.show();
+  mRef.child("Courses").child("Codes").addListenerForSingleValueEvent(new ValueEventListener() {
+   @Override
+   public void onDataChange(DataSnapshot dataSnapshot) {
+    courseCodes = (ArrayList<String>) dataSnapshot.getValue();
+    Toast.makeText(c, "Course codes found...", Toast.LENGTH_SHORT).show();
+    progressDialog.dismiss();
 
-                Toast.makeText(c, "Unable to get course codes...", Toast.LENGTH_SHORT).show();
-                progressDialog.dismiss();
-            }
-        });
+   }
 
-    }
+   @Override
+   public void onCancelled(DatabaseError databaseError) {
 
-    public ArrayList<String> getCourseCodes() {
-        return courseCodes;
-    }
+    Toast.makeText(c, "Unable to get course codes...", Toast.LENGTH_SHORT).show();
+    progressDialog.dismiss();
+   }
+  });
+
+ }
+
+ public ArrayList<String> getCourseCodes() {
+  return courseCodes;
+ }
+
+ public String getDbLastName() {
+  return dbLastName;
+ }
+
+ public String getDbEmail() {
+  return dbEmail;
+ }
+
+ public String getDbPassword() {
+  return dbPassword;
+ }
+
+ public String getDbStudentNumber() {
+  return dbStudentNumber;
+ }
+
+
+
 }
